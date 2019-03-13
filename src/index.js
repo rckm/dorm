b40f93aa72bd2a0ee628ad04bbc75ea88829a01b
blitzero <erik.kim2013@yandex.kz>
2019-03-14 01:09:17 +0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./components/App";
 import APIContext, { API } from "./components/API";
-// import createHistory from "history/createBrowserHistory";
+import createHistory from "history/createBrowserHistory";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 
-// const history = createHistory({
-//   basename: process.env.PUBLIC_URL
-// });
+const history = createHistory({
+  basename: process.env.PUBLIC_URL
+});
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter history={history} basename={process.env.PUBLIC_URL}>
     <APIContext.Provider value={new API()}>
       <App />
     </APIContext.Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
